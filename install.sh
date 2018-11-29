2bb72709c7faaef56824045c92f8046569555c8e
Cat Prisbrey <catprisbrey@gmail.com>
2018-11-29 16:29:44 -0700

    Fixed installer syntax issues

--- install.sh
+++ install.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
+sudo apt-get update -qq
+
 sudo apt-get install -qq yad 
 sudo apt-get install -qq hfsprogs
 
-LINUX=$?
-if [[ $LINUX eq -1 ]]; do
+if [ $? -eq 0 ]; then
 
+	echo "Dependencies have installed and Mirrokat is ready for use!"
+	read -n 1 -srp "Press any key to exit"
+	echo 
+	exit
 
 else
 
-sudo dnf install -qy yad
-sudo dnf install -qy hfsplus-tools
+	sudo dnf install -qy yad
+	sudo dnf install -qy hfsplus-tools
+
+		if [ $? -eq 0 ]; then
+
+		echo "Dependencies have installed and Mirrokat is ready for use!"
+		read -n 1 -srp "Press any key to exit"
+		echo 
+		exit
+
+		fi
+	fi
+
+echo "Something went wrong..."
+
+read -n 1 -srp "Press any key to exit"
+echo 
+exit
